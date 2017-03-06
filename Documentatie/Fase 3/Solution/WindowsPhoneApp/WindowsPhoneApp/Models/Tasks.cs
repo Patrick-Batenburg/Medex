baa3465427ae8c8dd845a10ed13b7d57880c3923
Patrick <zammypatrick@gmail.com>
2017-03-06 11:25:00 +0100

    DatabaseProvider klaar en models af

---
+++ Documentatie/Fase 3/Solution/WindowsPhoneApp/WindowsPhoneApp/Models/Tasks.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsPhoneApp.Models
+{
+    class Tasks
+    {
+        [SQLite.AutoIncrement, SQLite.PrimaryKey]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Duration { get; set; }
+        public string Description { get; set; }
+        public string Remarks { get; set; }
+        public decimal Costs { get; set; } 
+    }
+}
